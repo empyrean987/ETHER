dc588cf8222b7e7df6b01174585dc7f0fc963a74
Michael Knox <knoxm1@gmail.com>
2018-01-13 17:40:35 -0600

    adding cuda support

--- cookbooks/ETHER/recipes/default.rb
+++ cookbooks/ETHER/recipes/default.rb
@@ -30,3 +30,26 @@ apt_update 'update'
 apt_package 'cpp-ethereum' do
   action :install
 end
+
+remote_file '/home/ubuntu/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64' do
+  source 'https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+dpkg_package 'cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64' do
+  source '/home/ubuntu/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64'
+  action :install
+end
+
+execute 'apt_key' do
+  command 'apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub'
+end
+
+apt-update 'update'
+
+apt_package 'cuda' do
+  action :install
+end
