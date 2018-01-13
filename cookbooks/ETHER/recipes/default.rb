2b3a7f199aa92d42c0bb95ced467bc9519e696a6
Michael Knox <knoxm1@gmail.com>
2018-01-12 19:38:47 -0600

    updating ether cookbook and removing graphite

--- cookbooks/ETHER/recipes/default.rb
+++ cookbooks/ETHER/recipes/default.rb
@@ -6,3 +6,21 @@
 apt_package 'software-properties-common' do
   action :install
 end
+
+apt_repository 'ethereum-qt' do
+  uri        'http://ppa:ethereum/ethereum-qt'
+end
+
+apt_repository 'ethereum' do
+  uri        'http://ppa:ethereum/ethereum'
+end
+
+apt_repository 'ethereum-dev' do
+  uri        'http://ppa:ethereum/ethereum-dev'
+end
+
+apt_update 'update'
+
+apt_package 'ethereum' do
+  action :install
+end
