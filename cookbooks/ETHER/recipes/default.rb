419e85f6261199cbc9165c67d92887a1b47441bd
Odyssey <knoxm1@Odysseys-iMac.lan>
2018-06-16 23:43:58 -0500

    updating chef recipe to comment out nvidia section

--- cookbooks/ETHER/recipes/default.rb
+++ cookbooks/ETHER/recipes/default.rb
@@ -19,12 +19,6 @@ apt_repository 'ethereum-dev' do
   uri        'ppa:ethereum/ethereum-dev'
 end
 
-apt_update 'update'
-
-apt_package 'nvidia-384' do
-  action :install
-end
-
 apt_package 'ethereum' do
   action :install
 end
@@ -33,20 +27,28 @@ apt_package 'cpp-ethereum' do
   action :install
 end
 
-apt_package 'xorg' do
-  action :install
-end
+#####NVIDIA Section, uncomment
+#apt_update 'update'
 
-apt_package 'lightdm' do
-  action :install
-end
+#apt_package 'nvidia-384' do
+#  action :install
+#end
+
+#apt_package 'xorg' do
+#  action :install
+#end
+
+#apt_package 'lightdm' do
+#  action :install
+#end
+
 
 #execute 'nvidia_gpus' do
 #  command 'nvidia-xconfig --enable-all-gpus'
 #end
 
 #execute 'nvidia_cool' do
-#  command 'nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration'
+#  command 'nvidia-xconfig -a --force-generate --allow-empty-initial-configuration --cool-bits=28 --no-sli --connected-monitor="DFP-0"'
 #end
 
 #directory '/var/run/nvidia-persistenced' do
@@ -59,30 +61,9 @@ end
 #service 'lightdm' do
 #  action :start
 #end
-#remote_file '/home/ubuntu/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64' do
-#  source 'https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64'
-#  owner 'root'
-#  group 'root'
-#  mode '0755'
-#  action :create
-#end
 
-#dpkg_package 'cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64' do
-#  source '/home/ubuntu/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64'
-#  action :install
-#end
+#AMD SECTION
 
-#execute 'apt_key' do
-#  command 'apt-key add /var/cuda-repo-9-1-local/7fa2af80.pub'
-#end
-
-#execute 'aptupdate' do
-#  command 'apt-get update'
-#end
-
-#apt_package 'cuda' do
-#  action :install
-#end
 
 apt_package 'awscli' do
   action :install
